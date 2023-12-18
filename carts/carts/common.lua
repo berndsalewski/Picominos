bd13562ba43969d801e8202cfaea4fa454201981
Bernd Salewski <bernd.salewski@gmail.com>
2023-12-18 14:40:48 +0100

    add "print"array" and helper enums to common.lua

--- carts/carts/common.lua
+++ carts/carts/common.lua
@@ -13,3 +13,28 @@ function vec2.new(o)
     setmetatable(o,self)
     return o
 end
+
+--prints the content of a 1-based integer index table
+--does not extract table values
+function print_array(t,x,y,col)
+    if(t==nil)return 
+	local out = "{"
+	for val in all(t) do
+        if type(val) == "table" then
+            out=out.."table,"
+        else
+            out=out..val..","
+        end
+	end
+	out=sub(out,1,#out-1).."}"
+	print(out,50,50,col)
+end
+
+--enum for color values
+e_colors={
+    pitchblue=0,darkblue=1,purple=2,earthlygreen=3,
+    brickbrown=4,mudbrown=5,lightgray=6,white=7,
+    red=8,orange=9,yellow=10,green=11,
+    blue=12,violet=13,pink=14,peach=15
+}
+e_buttons={left=0,right=1,up=2,down=3,o=4,x=5}
